6ed00df0b8da79e6e3bb14ce675650cd4925ff3c
C4Jay <choolakejay@gmail.com>
2019-11-05 18:49:54 +0530

    parti

--- src/components/HomeTikl.vue
+++ src/components/HomeTikl.vue
@@ -1,7 +1,29 @@
 <template>
+<div class="ikls">
     <div id="app">
-      <vue-particles color="#4ee810"></vue-particles>
+      <div class="ikls2">
+      <vue-particles color="#66f542"
+        :particleOpacity="0.7"
+        linesColor="#66f542"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="5"
+        :linesWidth="2"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"></vue-particles>
+      </div>
+       
+       
+  <!--      background-size: cover;
+    position: absolute;  -->
     </div>
+</div>
  </template>
 
 
@@ -10,3 +32,29 @@ export default {
     name: 'HomeTikl'
 }
  </script>
+
+
+<style>
+
+
+ .ikls2 {
+   height: 1200px;
+ }
+
+  
+  .ikls {
+    background-image: url("https://i.imgur.com/ZvsH8or.jpg");
+  
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  
+  
+
+
+ 
+
+</style>
