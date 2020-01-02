0ec0bc03d3e1c17056cfe1147374820e7c6eea5e
C4Jay <choolakejay@gmail.com>
2020-01-02 20:40:15 +0530

    bar-working

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -2,13 +2,13 @@
     <span>
         <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
             <v-list>
-                <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
+                <template v-for="item in items">
+                    <v-list-tile :key="item.title">
                         <v-list-tile-content>
                             {{item.title}}
                         </v-list-tile-content>
                     </v-list-tile>
-                    <v-divider :key="`divider-${index}`"></v-divider>
+                    <v-divider :key="`divider-${item.title}`"></v-divider>
                 </template>
             </v-list>
         </v-navigation-drawer>
@@ -18,18 +18,20 @@
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <router-link to="/">
             
-                <v-toolbar-title data-cy="titleBtn">{{appTitle}}</v-toolbar-title>
+                <v-toolbar-title data-cy="titleBtn" class="blue lighten-1">{{appTitle}}</v-toolbar-title>
             </router-link>
        <!--     <v-btn flat class="hidden-sm-and-down nav-menu" to="/menu" data-cy="menuBtn">Menu</v-btn>  -->
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <div v-if="!isAuthenticated" class="hidden-sm-and-down">
-                <v-btn flat color="brown lighten-3" to="/sign-in" data-cy="signinBtn">SIGN IN</v-btn>
-                <v-btn flat color="brown lighten-3" to="/signup" class="nav-join" data-cy="joinBtn">JOIN</v-btn>
-            </div>
-            <div v-else>
-                <v-btn flat to="/about">PROFILE</v-btn>
-                <v-btn outline color="white" @click="logout" data-cy="logout">Logout</v-btn>
-            </div>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn
+                flat
+                v-for="item in items"
+                :key="item.title"
+                :to="item.to"
+                >
+                {{item.title}}
+                </v-btn>
+            </v-toolbar-items>
         </v-toolbar>
     </span>
 </template>
@@ -41,24 +43,32 @@ export default {
         return {
             appTitle: 'Treflor',
             drawer: false,
-            items: [
-                { title: 'Menu' },
-                { title: 'Profile' },
-                { title: 'Sign In' },
-                { title: 'Join' }
-            ]
+            
         };
     },
     computed: {
-        isAuthenticated() {
-            return this.$store.getters.isAuthenticated;
+        items () {
+            let items = [
+                {title: 'Sign Up' , to: '/signup'},                
+                {title: 'Sign In' , to: '/sign-in'},
+            ]
+
+            if(this.userIsauthenticated ){
+                items = [                    
+                {title: 'View Guides' , to: '/viewguides'},
+                {title: 'Create Guide' , to: '/newhike'},
+                {title: 'Profile' , to: '/profile'},
+
+                ]
+            }
+            return items
+        },
+
+        userIsauthenticated () {
+            return this.$store.getters.user !== null && this.$store.getters.user !== undefined
         }
     },
-    methods: {
-        logout() {
-            this.$store.dispatch('userSignOut');
-        }
-    }
+   
 };
 </script>
 
