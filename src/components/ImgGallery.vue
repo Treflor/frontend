1975d5f03c49983d19ab52fa537a295bb5c064a9
C4Jay <choolakejay@gmail.com>
2020-01-10 09:45:47 +0530

    imgs-click-work-css-work

--- src/components/ImgGallery.vue
+++ src/components/ImgGallery.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container>
-    <v-layout row wrap>
-    <v-flex xs12 mb-8 v-for="gallery in galleries" :key="gallery.title">
-        <v-card color="#f5c242">
-            <v-card-title class="font-weight-black">{{ gallery.title }}</v-card-title>
-            <v-layout row wrap>
+    <v-layout row wrap mb-8>
+    <v-flex xs12  v-for="gallery in galleries" :key="gallery.title">
+        <v-card color="#f5c242" >
+            <v-card-title class="font-weight-black headline">{{ gallery.title }}</v-card-title>
+            <v-layout row wrap class="text-xs-center">
                 <v-flex xs12 sm3 mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img1"></expandable-image>
                 </v-flex>
@@ -14,29 +14,29 @@
                 <v-flex xs12 sm3 mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img3"></expandable-image>
                 </v-flex >
-                 <v-flex xs12 sm3 v-if="gallery.img4 != null" xs4 sm4 mb-2 ml-1 mr-1>
+                 <v-flex xs12 sm3 v-if="gallery.img4 != null"  mb-2 ml-1 mr-1>
                      <expandable-image :src="gallery.img4"></expandable-image>
                 </v-flex>
-                <v-flex xs12 sm3 v-if="gallery.img5 != null" xs4 sm4 mb-2 ml-1 mr-1>
+                <v-flex xs12 sm3 v-if="gallery.img5 != null" mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img5"></expandable-image>
                 </v-flex>
-                <v-flex xs12 sm3 v-if="gallery.img6 != null" xs4 sm4 mb-2 ml-1 mr-1>
+                <v-flex xs12 sm3 v-if="gallery.img6 != null" mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img6"></expandable-image>
                 </v-flex>
                 
-                <v-flex xs12 sm3 v-if="gallery.img7 != null" xs4 sm4 mb-2 ml-1 mr-1>
+                <v-flex xs12 sm3 v-if="gallery.img7 != null" mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img7"></expandable-image>
                 </v-flex>
                 
-                <v-flex xs12 sm3 v-if="gallery.img8 != null" xs4 sm4 mb-2 ml-1 mr-1>
+                <v-flex xs12 sm3 v-if="gallery.img8 != null" mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img8"></expandable-image>
                 </v-flex>
                 
-                <v-flex xs12 sm3 v-if="gallery.img9 != null" xs4 sm4 mb-2 ml-1 mr-1>
+                <v-flex xs12 sm3 v-if="gallery.img9 != null" mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img9"></expandable-image>
                 </v-flex>
                 
-                <v-flex xs12 sm3 v-if="gallery.img10 != null" xs4 sm4 mb-2 ml-1 mr-1>
+                <v-flex xs12 sm3 v-if="gallery.img10 != null" mb-2 ml-1 mr-1>
                     <expandable-image :src="gallery.img10"></expandable-image>
                 </v-flex>
             </v-layout>
